5af6ed27d2671d8c1d242b55acc904a70f49095b
AndreaPiazza33 <andreapiazza190201@gmail.com>
2023-08-31 15:45:07 +0200

    feat: add to do list

---
+++ JS/main.js
@@ -0,0 +1,42 @@
+//TO DO LIST
+const toDo = [
+    {
+        title:'prendere il giornale',
+        done: false,
+    },
+    {
+        title:'fare la spesa',
+        done: true,
+    },
+    {
+        title:'portare fuori il cane',
+        done: true,
+    },
+    {
+        title:'preparare da mangiare',
+        done: false,
+    },
+    {
+        title:'tagliare il prato',
+        done: false,
+    },
+    {
+        title:'andare a camminare',
+        done: true,
+    },
+    {
+        title:'leggere un libro',
+        done: true,
+    },
+]
+
+//VUE
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+        toDo
+    }
+  }
+}).mount('#app')
